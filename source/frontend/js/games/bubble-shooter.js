95dd1106b05a84e10492db9c09fec1ec557ac95f
Paul Nichols <paul.rene.nichols@gmail.com>
2015-08-16 17:56:09 -0500

    Adding initial code for bubble shooter game logic.

--- source/frontend/js/games/bubble-shooter.js
+++ source/frontend/js/games/bubble-shooter.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
+
+  var BubbleShoot = window.BubbleShoot || {};
+
+  function buildGame () {
+
+    var game = {};    
+
+    function initialize () {
+      $(".button_start_game").bind("click", startGame);
+    };
+    
+    function startGame() {
+    };
+
+    game.initialize = initialize;
+
+    return game;
   
+  }
+
+  BubbleShoot.Game = buildGame();
+
+  BubbleShoot.Game.initialize();
 });
