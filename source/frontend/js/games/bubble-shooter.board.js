f77585641e295b062839481e7d7a648ef57ce6aa
Paul Nichols <paul.rene.nichols@gmail.com>
2015-08-22 18:36:15 -0500

    Fixed linting issue about '=='.  Made it '==='.

--- source/frontend/js/games/bubble-shooter.board.js
+++ source/frontend/js/games/bubble-shooter.board.js
@@ -21,17 +21,17 @@ $(document).ready(function () {
       for(var i=0; i<NUM_ROWS; i++){
 
         var row = [];
-        var startCol = (i % 2 == 0) ? 1 : 0;
+        var startCol = (i % 2 === 0) ? 1 : 0;
         for(var j=startCol; j<NUM_COLS; j+=2){
           var bubble = BubbleShoot.Bubble.create(i,j);
           row[j] = bubble;
-        };
+        }
         rows.push(row);
-      };
+      }
       return rows;
     }
 
-    board.getRows = getRows
+    board.getRows = getRows;
 
     return board;
   
