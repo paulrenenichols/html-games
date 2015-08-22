96ed84e4f3258c47cc2af8dcd137350304721881
Paul Nichols <paul.rene.nichols@gmail.com>
2015-08-22 15:02:09 -0500

    Adding more url and api test code to index.js.

--- source/frontend/js/index.js
+++ source/frontend/js/index.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
   console.log(window.location.href);
+  console.log(window.location.host);
+  console.log(window.location.origin);
+
+  $.ajax({
+    url: window.location.origin + "/api/"
+  })
+  .done(function(data) {
+    console.log(data);
+  });
+
+  $.ajax({
+    url: window.location.origin + "/api/hello",
+    data: {
+      name: "bob"
+    }
+  })
+  .done(function(data) {
+    console.log(data.hello);
+  });
 });
