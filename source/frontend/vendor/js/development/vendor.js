3a40b395a48a2fd88c3d16e87a09071507784154
Paul Nichols <paul.rene.nichols@gmail.com>
2015-08-25 10:46:53 -0500

    Added comments and includes for Q and lodash to vendor.js bundle file.

--- source/frontend/vendor/js/development/vendor.js
+++ source/frontend/vendor/js/development/vendor.js
@@ -1,1 +1,12 @@
+/*
+ *
+ *  vendor.js
+ *
+ *  This file exists to create a browserify bundle for all
+ *  third party scripts.  The required scripts in here will 
+ *  be referenced (externally) by other bundles.
+ *
+ */
 var $ = require("jquery");
+var Q = require("q");
+var _ = require("lodash");
