75e82fbdec842e0411de05ded9d9da62ec4f221d
Paul Nichols <paul.rene.nichols@gmail.com>
2015-08-26 14:09:03 -0500

    Fully browserified the bubble shooter source code.

--- source/frontend/js/games/bubble-shooter.bubble.js
+++ source/frontend/js/games/bubble-shooter.bubble.js
@@ -1,71 +1,69 @@
-$(document).ready(function () {
+var $ = require('jquery');
+var ui = require('./bubble-shooter.ui.js');
 
-  var BubbleShoot = window.BubbleShoot = window.BubbleShoot || {};
+function create (row, column, type) {
 
-  function create (row, column, type) {
+  var bubble = {};
 
-    var bubble = {};
-
-    if(type === undefined){
-      type = Math.floor(Math.random() * 4);
-    }
-
-    var sprite = $(document.createElement("div"));
-    sprite.addClass("bubble");
-    sprite.addClass("bubble_" + type);
-
-    function getSprite() {
-      return sprite;
-    }
-
-    function position () {
-      return sprite.position();
-    }
-
-    function animate() {
-      return sprite.animate.apply(sprite, arguments);
-    }
-
-    function getType() {
-      return type;
-    }
-
-    function getRow() {
-      return row;
-    }
-
-    function getColumn() {
-      return column;
-    }
-
-    function getCoordinates(){
-      var coordinates = {
-        left: getColumn() * BubbleShoot.UI.BUBBLE_DIAMETER/2  + BubbleShoot.UI.BUBBLE_DIAMETER/2,
-        top:  getRow()    * BubbleShoot.UI.ROW_HEIGHT     + BubbleShoot.UI.BUBBLE_DIAMETER/2
-      };
-      return coordinates;
-    }
-
-    function destroy() {
-      console.log('bubble destroy');
-      sprite.remove();
-      sprite = null;
-    }
-
-
-    bubble.getSprite      = getSprite;
-    bubble.position       = position;
-    bubble.animate        = animate;
-    bubble.getType        = getType;
-    bubble.getRow         = getRow;
-    bubble.getColumn      = getColumn;
-    bubble.getCoordinates = getCoordinates;
-    bubble.destroy        = destroy;
-
-    return bubble;
+  if(type === undefined){
+    type = Math.floor(Math.random() * 4);
   }
 
-  BubbleShoot.Bubble        = {};
-  BubbleShoot.Bubble.create = create;
+  var sprite = $(document.createElement("div"));
+  sprite.addClass("bubble");
+  sprite.addClass("bubble_" + type);
 
-});
+  function getSprite() {
+    return sprite;
+  }
+
+  function position () {
+    return sprite.position();
+  }
+
+  function animate() {
+    return sprite.animate.apply(sprite, arguments);
+  }
+
+  function getType() {
+    return type;
+  }
+
+  function getRow() {
+    return row;
+  }
+
+  function getColumn() {
+    return column;
+  }
+
+  function getCoordinates(){
+    var coordinates = {
+      left: getColumn() * ui.BUBBLE_DIAMETER/2  + ui.BUBBLE_DIAMETER/2,
+      top:  getRow()    * ui.ROW_HEIGHT     + ui.BUBBLE_DIAMETER/2
+    };
+    return coordinates;
+  }
+
+  function destroy() {
+    console.log('bubble destroy');
+    sprite.remove();
+    sprite = null;
+  }
+
+
+  bubble.getSprite      = getSprite;
+  bubble.position       = position;
+  bubble.animate        = animate;
+  bubble.getType        = getType;
+  bubble.getRow         = getRow;
+  bubble.getColumn      = getColumn;
+  bubble.getCoordinates = getCoordinates;
+  bubble.destroy        = destroy;
+
+  return bubble;
+}
+
+module.exports = {
+  create: create
+};
--- source/frontend/js/games/bubble-shooter.game.js
+++ source/frontend/js/games/bubble-shooter.game.js
@@ -1,74 +1,73 @@
-$(document).ready(function () {
+var $ = require('jquery');
+var ui = require('./bubble-shooter.ui.js');
+var createBubble = require('./bubble-shooter.bubble.js').create;
+var board = require('./bubble-shooter.board.js');
+var collisionDetector = require('./bubble-shooter.collision.js');
 
-  var BubbleShoot = window.BubbleShoot = window.BubbleShoot || {};
+function buildGame () {
 
-  function buildGame () {
+  var game = {};    
 
-    var game = {};    
+  var userBubble;
+  var userBubbleCount;
+  var MAX_USER_BUBBLES = 70;
 
-    var userBubble;
-    var userBubbleCount;
-    var MAX_USER_BUBBLES = 70;
-
-    function initialize () {
-      $(".button_start_game").bind("click", startGame);
-    }
-
-    function mouseClickHandler(event){
-      var angle = BubbleShoot.UI.getBubbleAngle(userBubble, event);
-      var duration = 750;
-      var distance = 1000;
-      var collision = BubbleShoot.CollisionDetector.findIntersection(userBubble, game.board, angle);
-      var coords;
-      if(collision) {
-        console.log('collision', collision);
-        coords = collision.coords;
-        duration = Math.round(duration * collision.distToCollision / distance);
-      }
-      else {
-        var distX = Math.sin(angle) * distance;
-        var distY = Math.cos(angle) * distance;
-        var bubbleCoords = BubbleShoot.UI.getBubblePosition(userBubble);
-        coords = {
-          x : bubbleCoords.left + distX,
-          y : bubbleCoords.top - distY
-        };
-      }
-      BubbleShoot.UI.fireBubble(userBubble, coords, duration);
-      userBubble = createNextUserBubble();
-    }
-    
-    function createNextUserBubble () {
-      var bubble = BubbleShoot.Bubble.create();
-      bubble.getSprite().addClass("user_bubble");
-      $("#board").append(bubble.getSprite());
-      BubbleShoot.UI.drawUserBubbleCount(userBubbleCount);
-      userBubbleCount--;
-      return bubble;
-    }
-
-    function startGame() {
-      $(".button_start_game").unbind("click");
-      BubbleShoot.UI.hideDialog();
-      userBubbleCount = MAX_USER_BUBBLES;
-      userBubble = createNextUserBubble();
-
-
-      // Create the game board
-      game.board = BubbleShoot.Board.buildBoard();
-      BubbleShoot.UI.drawBoard(game.board);
-      
-      $("#game").bind("click", mouseClickHandler);
-
-    }
-
-    game.initialize = initialize;
-
-    return game;
-  
+  function initialize () {
+    $(".button_start_game").bind("click", startGame);
   }
 
-  BubbleShoot.Game = buildGame();
+  function mouseClickHandler(event){
+    var angle = ui.getBubbleAngle(userBubble, event);
+    var duration = 750;
+    var distance = 1000;
+    var collision = collisionDetector.findIntersection(userBubble, game.board, angle);
+    var coords;
+    if(collision) {
+      console.log('collision', collision);
+      coords = collision.coords;
+      duration = Math.round(duration * collision.distToCollision / distance);
+    }
+    else {
+      var distX = Math.sin(angle) * distance;
+      var distY = Math.cos(angle) * distance;
+      var bubbleCoords = ui.getBubblePosition(userBubble);
+      coords = {
+        x : bubbleCoords.left + distX,
+        y : bubbleCoords.top - distY
+      };
+    }
+    ui.fireBubble(userBubble, coords, duration);
+    userBubble = createNextUserBubble();
+  }
+  
+  function createNextUserBubble () {
+    var bubble = createBubble();
+    bubble.getSprite().addClass("user_bubble");
+    $("#board").append(bubble.getSprite());
+    ui.drawUserBubbleCount(userBubbleCount);
+    userBubbleCount--;
+    return bubble;
+  }
 
-  BubbleShoot.Game.initialize();
-});
+  function startGame() {
+    $(".button_start_game").unbind("click");
+    ui.hideDialog();
+    userBubbleCount = MAX_USER_BUBBLES;
+    userBubble = createNextUserBubble();
+
+
+    // Create the game board
+    game.board = board.buildBoard();
+    ui.drawBoard(game.board);
+    
+    $("#game").bind("click", mouseClickHandler);
+
+  }
+
+  game.initialize = initialize;
+
+  return game;
+
+}
+
+module.exports = buildGame;
