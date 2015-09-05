93a9a1344408f5d70ac08f7079030e096cfd95b2
Paul Nichols <paul.rene.nichols@gmail.com>
2015-09-05 14:18:54 -0500

    Added documentation to engine.js

--- source/frontend/js/engine/engine.js
+++ source/frontend/js/engine/engine.js
@@ -1,2 +1,10 @@
+// # engine.js
 
+// This is the game engine's [Browserify](https://github.com/substack/node-browserify) bundle file.
+//
+// The various game engine libraries are bundled here so that they can be used by games.
+
+// **vector2d**
+//
+// This is our 2 dimensional vector library.
 var vector2d = require('./vector2d.js');
