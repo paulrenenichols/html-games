9ba49f5f8e06833c8b393ac36b8ad35b8f74fb30
Paul Nichols <paul.rene.nichols@gmail.com>
2015-09-06 17:42:08 -0500

    Added debugging to bubble-shooter.js

--- source/frontend/js/games/bubble-shooter.js
+++ source/frontend/js/games/bubble-shooter.js
@@ -1,4 +1,8 @@
 var $ = require('jquery');
 var game = require('./bubble-shooter.game.js')();
 
+var debug = require('./bubble-shooter.debug.js')('bubble-shooter:main');
+
+debug('Inializing bubble shooter');
+
 game.initialize();
